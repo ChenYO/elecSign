a5cfe7c79878dc8dd235ba1b2e24f3cf00d9f6b5
Chen Yao <loxa0802@gmail.com>
2020-03-19 08:50:51 +0800

    Initial Commit

---
+++ elecSign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  elecSign
+//
+//  Created by 陳仲堯 on 2020/3/19.
+//  Copyright © 2020 陳仲堯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
